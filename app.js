9c264759bef105f7af14e4dc0f4e20d7ececd69b
Aqib Javid Bhat <aqib.m31.dev@gmail.com>
2023-12-31 15:33:57 +0530

    Turn the text to h1 heading.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 http.createServer(function (req, res){
-	res.write('On the way to be a full snack engineer!');
+	res.write('<h1>On the way to be a full snack engineer!</h1>');
 	res.end();
 }).listen(3000);
 console.log('Server Started on port 3000.');
